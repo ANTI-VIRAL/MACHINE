aca30b08d991bafa0761d4e5136d1da6fc0c2d70
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-03-15 15:17:41 +0800

    Update view.py

--- view.py
+++ view.py
@@ -1,25 +1,9 @@
+import os
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
-# Setup Chrome di Headless Mode
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Tanpa GUI
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--mute-audio")  # Biar nggak bunyi
-chrome_options.add_argument("--window-size=1280,720")
-
-# Install dan jalankan Chrome
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-# URL YouTube ayang
-youtube_url = "https://www.youtube.com/watch?v=3oTxP-a0rnE"
+YOUTUBE_URL = "https://www.youtube.com/watch?v=3oTxP-a0rnE"
 
 while True:
-    driver.get(youtube_url)  # Buka video
-    print("🎥 Nonton video:", youtube_url)
-    time.sleep(3600)  # Nonton 1 jam sebelum refresh
+    os.system(f"nohup yt-dlp -q -o - {YOUTUBE_URL} | ffplay -nodisp -autoexit - > /dev/null 2>&1 &")
+    print("🎥 Nonton YouTube di background...")
+    time.sleep(21600)  # Refresh tiap 6 jam
